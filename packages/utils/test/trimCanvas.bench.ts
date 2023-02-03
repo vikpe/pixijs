6f82cfe9d025c654e295506b311ed64ec5c7f47f
vi-per <viktor.persson@arcsin.se>
2023-02-03 21:56:30 +0100

    Refactor benchmark; move setup code.

--- packages/utils/test/trimCanvas.bench.ts
+++ packages/utils/test/trimCanvas.bench.ts
@@ -1,23 +1,23 @@
 import { benchmarkSuite } from 'jest-bench';
 import { trimCanvas } from '@pixi/utils';
 
+const canvas = document.createElement('canvas');
+
+canvas.width = 100;
+canvas.height = 50;
+
+const context = canvas.getContext('2d');
+
+if (context === null) fail('Failed to get canvas 2D context');
+
+context.fillStyle = '#ff0000';
+context.fillRect(10, 20, 10, 5);
+context.fillStyle = '#00ff00';
+context.fillRect(15, 25, 10, 5);
+
 benchmarkSuite('trimCanvas', {
     'non-empty canvas': () =>
     {
-        const canvas = document.createElement('canvas');
-
-        canvas.width = 100;
-        canvas.height = 50;
-
-        const context = canvas.getContext('2d');
-
-        if (context === null) fail('Failed to get canvas 2D context');
-
-        context.fillStyle = '#ff0000';
-        context.fillRect(10, 20, 10, 5);
-        context.fillStyle = '#00ff00';
-        context.fillRect(15, 25, 10, 5);
-
         trimCanvas(canvas);
     },
 });
